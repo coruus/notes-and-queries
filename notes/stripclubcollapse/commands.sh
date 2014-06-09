008867145a55379f869c4d17a567b109404201ad
David Leon Gil <coruus@gmail.com>
2014-06-09 09:41:19 -0400

    Collapse of the Cloakroom.

---
+++ notes/stripclubcollapse/commands.sh
@@ -0,0 +1,3 @@
+wget http://www.popville.com/wp-content/uploads/2014/05/photo-21-e1399066731185.jpg
+wget http://www.popville.com/wp-content/uploads/2014/05/BmqDIEFIMAAJmYf.jpg
+wget http://www.popville.com/wp-content/uploads/2014/05/BmqAW_FIYAAG_p5.jpg
